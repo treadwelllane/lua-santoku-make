89ebcaf4595ef80a221c450e1a7c0d2714840990
Matthew Brooks <mb964033@gmail.com>
2024-02-08 00:02:20 +0000

    bump libs

--- make.lua
+++ make.lua
@@ -1,18 +1,18 @@
 local env = {
 
   name = "santoku-make",
-  version = "0.0.38-1",
+  version = "0.0.39-1",
   variable_prefix = "TK_MAKE",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.179-1",
-    "santoku-fs >= 0.0.23-1",
-    "santoku-system >= 0.0.18-1",
-    "santoku-template >= 0.0.18-1",
-    "santoku-bundle >= 0.0.25-1",
+    "santoku >= 0.0.184-1",
+    "santoku-fs >= 0.0.25-1",
+    "santoku-system >= 0.0.20-1",
+    "santoku-template >= 0.0.19-1",
+    "santoku-bundle >= 0.0.26-1",
     "basexx >= 0.4.1-1"
   },
 

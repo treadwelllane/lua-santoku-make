b8b3132b45ff8750896002b1d2beb5354368df4a
Matthew Brooks <mb964033@gmail.com>
2025-12-05 18:18:17 -0500

    update

--- make.lua
+++ make.lua
@@ -1,17 +1,17 @@
 local env = {
   name = "santoku-make",
-  version = "0.0.159-1",
+  version = "0.0.160-1",
   variable_prefix = "TK_MAKE",
   license = "MIT",
   public = true,
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.297-1",
-    "santoku-fs >= 0.0.34-1",
-    "santoku-system >= 0.0.56-1",
-    "santoku-template >= 0.0.31-1",
-    "santoku-mustache >= 0.0.12-1",
-    "santoku-bundle >= 0.0.36-1",
+    "santoku >= 0.0.303-1",
+    "santoku-fs >= 0.0.37-1",
+    "santoku-system >= 0.0.58-1",
+    "santoku-template >= 0.0.32-1",
+    "santoku-mustache >= 0.0.13-1",
+    "santoku-bundle >= 0.0.37-1",
   },
   rules = {
     copy = {

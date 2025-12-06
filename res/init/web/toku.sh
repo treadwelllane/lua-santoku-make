bc154b5f80e5eec893b14a67ab6c13f632b01fe0
Matthew Brooks <mb964033@gmail.com>
2025-12-05 21:38:06 -0500

    update

--- res/init/web/toku.sh
+++ res/init/web/toku.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 podman build . --iidfile .toku.id
-podman run --entrypoint /bin/bash -p 8080:8080 -e DB_FILE="$DB_FILE" -ti -v "$(dirname "$0")":/app -w /app --rm $(cat .toku.id) "$@"
+podman run -p 8080:8080 -e DB_FILE="$DB_FILE" -ti -v "$(dirname "$0")":/app -w /app --rm $(cat .toku.id) "$@"

e5c1a3ab22959baf73fea54585fcdc44a30b5daa
Matthew Brooks <mb964033@gmail.com>
2025-12-10 19:40:45 -0500

    update

--- toku-web.sh
+++ toku-web.sh
@@ -4,13 +4,10 @@ set -e
 script_dir="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
 cmd=""
-while getopts "c:" opt; do
-  case $opt in
-    c) cmd="$OPTARG" ;;
-    *) echo "Usage: $0 [-c docker|podman] [docker-opts...] -- [args...]" >&2; exit 1 ;;
-  esac
-done
-shift $((OPTIND - 1))
+if [ "$1" = "-c" ]; then
+  cmd="$2"
+  shift 2
+fi
 
 docker_opts=""
 while [ $# -gt 0 ]; do
@@ -33,6 +30,11 @@ if [ -z "$cmd" ]; then
   fi
 fi
 
-mkdir -p build
-$cmd build -f "$script_dir"/toku-web.dockerfile . --iidfile build/.toku-web.id
-$cmd run $docker_opts -ti -v "$(pwd)":/app -w /app --rm $(cat build/.toku-web.id) "$@"
+cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/toku-web"
+mkdir -p "$cache_dir"
+$cmd build -f "$script_dir"/toku-web.dockerfile . --iidfile "$cache_dir"/image.id
+userns=""
+if [ "$cmd" = "podman" ]; then
+  userns="--userns=keep-id"
+fi
+$cmd run $userns $docker_opts -ti -v "$(pwd)":/app -w /app --rm $(cat "$cache_dir"/image.id) "$@"

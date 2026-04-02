76b0b8fb832d55a520da2f6d1f0a02c6695663b7
Matthew Brooks <mb964033@gmail.com>
2026-04-02 10:31:36 -0400

    update

--- make.lua
+++ make.lua
@@ -1,6 +1,6 @@
 local env = {
   name = "santoku-make",
-  version = "0.0.207-1",
+  version = "0.0.208-1",
   variable_prefix = "TK_MAKE",
   license = "MIT",
   public = true,
@@ -8,7 +8,7 @@ local env = {
     "lua == 5.1",
     "santoku >= 0.0.324-1",
     "santoku-fs >= 0.0.43-1",
-    "santoku-web >= 0.0.479-1",
+    "santoku-web >= 0.0.480-1",
     "santoku-system >= 0.0.62-1",
     "santoku-template >= 0.0.36-1",
     "santoku-mustache >= 0.0.14-1",
